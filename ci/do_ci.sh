d76d3096c4cbd647d26b44b3f801c3afbc81d3e2
Nicholas Johns <taion809@users.noreply.github.com>
2018-03-02 11:06:40 -0500

    Build: create initial circleci project (#41)
    This creates the initial build and test workflows for circleci
    
    Signed-off-by: <Nicholas Johns nicholas.a.johns5@gmail.com>

---
+++ ci/do_ci.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+if [[ -f "${HOME:-/root}/.gitconfig" ]]; then
+    mv "${HOME:-/root}/.gitconfig" "${HOME:-/root}/.gitconfig_save"
+fi
+
+function do_build () {
+    bazel build --verbose_failures=true //:envoy
+}
+
+function do_test() {
+    bazel test --test_output=all //:echo2_integration_test
+}
+
+case "$1" in
+  build)
+    do_build
+  ;;
+  test)
+    do_test
+  ;;
+  *)
+    echo "must be one of [build,test]"
+    exit 1
+  ;;
+esac
